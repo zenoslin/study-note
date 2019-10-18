cc8647e967dc9a6bcdc7d05ff9bb86127a77f7d4
zenoslin <zenoslin@outlook.com>
2019-10-18 14:05:36 +0800

    feat: :cake: 支持PWA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,18 +4,36 @@ module.exports = {
   dest: "./dist", // 设置输出目录
   base: "/study-note/",
   head: [
+    ["link", { rel: "icon", href: "/logo.png" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     [
       "meta",
       { name: "apple-mobile-web-app-status-bar-style", content: "black" }
     ],
     [
+      "link",
+      { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.png" }
+    ],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/icons/safari-pinned-tab.svg",
+        color: "#3eaf7c"
+      }
+    ],
+    [
       "meta",
       {
-        name: "description",
-        itemprop: "description",
-        content: "一些学习笔记"
+        name: "msapplication-TileImage",
+        content: "/icons/msapplication-icon-144x144.png"
       }
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }],
+    [
+      "meta",
+      { name: "description", itemprop: "description", content: "一些学习笔记" }
     ]
   ],
   repo: "https://github.com/zenoslin/study-note", // 添加 github 链接
@@ -49,14 +67,15 @@ module.exports = {
       {
         title: "工具",
         collapsable: false,
-        children: [
-          ["/emoji/", "emoji list"],
-          ["/atlas/", "图集打包工具"]
-        ]
+        children: [["/emoji/", "emoji list"], ["/atlas/", "图集打包工具"]]
       }
     ]
   },
   markdown: {
     lineNumbers: true
-  }
+  },
+  plugins: ['@vuepress/pwa', {
+    serviceWorker: true,
+    updatePopup: true
+  }],
 };
